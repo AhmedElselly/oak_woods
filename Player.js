08693f7baad884e5a14368b23c28d4fb7d8a67a7
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-05-11 16:51:16 +0300

    changed collisions

--- Player.js
+++ Player.js
@@ -6,7 +6,6 @@ class Player {
     this.h = this.spriteHeight;
     this.x = 0;
     this.y = 0;
-    this.vy = 0;
     this.pos = createVector(this.x + 100, this.y);
     this.minFrame = 0;
     this.maxFrame = 22;
@@ -18,7 +17,8 @@ class Player {
     this.staggerFrame = 5;
     this.mass = 5;
     this.vel = createVector(0, 0);
-    this.gravity = createVector(0, 1);
+    this.gravity = createVector(0, 0.8);
+    this.g = 0.5;
     this.acc = createVector(0, 0);
     this.spriteAnimations = [];
     this.animationStates = [
@@ -35,14 +35,6 @@ class Player {
         frames: 6,
       },
     ];
-  }
-
-  applyForce() {
-    let a = p5.Vector.div(this.gravity, this.mass);
-    this.acc.add(a);
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.set(0, 0);
   }
 
   getState() {
@@ -70,47 +62,59 @@ class Player {
   }
 
   stop() {
-    this.acc.set(0, 0);
     this.gravity.set(0, 0);
     this.vel.set(0, 0);
   }
 
+  applyForce() {
+    // this.gravity.set(0, 0.8)
+    let a = p5.Vector.div(this.gravity, this.mass);
+    this.acc.add(a);
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  grounded() {
+    let diffHeight = height - this.pos.y;
+    console.log({ diffHeight });
+    return this.pos.y === height - this.h;
+  }
+
+  jump() {
+    console.log(this.grounded());
+    // if (this.grounded()) {
+    this.vel.add(0, -8);
+    this.gravity.set(0, 1);
+    this.pos.add(this.vel);
+    this.vel.add(this.gravity);
+    this.pos.y = constrain(this.pos.y, 0, height - this.h);
+    // }
+  }
+
   movement(key) {
+    // console.log(this.gravity)
     if (key === "d") {
       let speed = 0.5;
-      let right = createVector(1 * speed, 0);
+      let right = createVector(speed, 0);
       this.vel.add(right);
       this.pos.add(this.vel);
+      // this.vel.set(0, 0)
     }
     if (key === "a") {
       let speed = 0.5;
-      let left = createVector(-1 * speed, 0);
+      let left = createVector(-speed, 0);
       this.vel.add(left);
       this.pos.add(this.vel);
-    }
-    if (key === "space") {
-      
-      let up = createVector(0, -1);
-      this.vel.add(up);
-      this.pos.add(this.vel);
-      // console.log(this.pos.y, height - this.h)
-      // if(!this.checkJump()) {
-      //   this.applyForce()
-      // }
     }
   }
 
   checkJump(boundary) {
-    console.log(this.pos.y, boundary.h, this.h);
-    // this.vy++;
-    // this.pos.add(0, this.vy);
-    
     if (this.pos.y < boundary.h + 230) {
       // this.stop()
       let up = createVector(0, 1);
       this.vel.add(up);
       this.pos.add(this.vel);
-      
     }
   }
 
--- script.js
+++ script.js
@@ -8,10 +8,10 @@ let boundaries = [];
 let keys = {
   d: false,
   a: false,
-  space: false
-}
+  space: false,
+};
 
-let pressedKey = '';
+let pressedKey = "";
 
 for (let i = 0; i < collisions.length; i += 30) {
   collisionsMap.push(collisions.slice(i, i + 30));
@@ -22,20 +22,32 @@ function preload() {
   charImage = loadImage("assets/character/char_blue.png");
 }
 
-function rectangularCollision(rec1, rec2) {
-  return (
-    rec1.pos.x + rec1.w >= rec2.pos.x &&
-    rec1.pos.x <= rec2.pos.x + rec1.w &&
-    rec1.pos.y <= rec2.pos.y + rec1.h &&
-    rec1.pos.y + rec1.h >= rec2.pos.y
+function collided(rect1, rect2) {
+  return collideRectRect(
+    rect1.pos.x,
+    rect1.pos.y,
+    rect1.w,
+    rect1.h,
+    rect2.pos.x,
+    rect2.pos.y,
+    rect2.w,
+    rect2.h
   );
 }
 
+function detectCollisions(player, boundary) {
+  return (
+    player.pos.y + player.h < boundary.pos.y &&
+    player.pos.y + player.h + player.vel.y >= boundary.pos.y && 
+    player.pos.x + player.w >= boundary.pos.x && collided(player, boundary)
+  )
+}
+
 function keyPressed() {
-  console.log(key)
+  console.log(key);
   keys[key] = true;
-  if(key === ' ') {
-    pressedKey = 'space';
+  if (key === " ") {
+    player.jump();
   } else {
     pressedKey = key;
   }
@@ -43,8 +55,8 @@ function keyPressed() {
 
 function keyReleased() {
   keys[key] = false;
-  pressedKey = '';
-  player.applyForce()
+  pressedKey = "";
+  player.applyForce();
 }
 
 function setup() {
@@ -52,7 +64,7 @@ function setup() {
   collisionsMap.forEach((row, i) => {
     row.forEach((source, j) => {
       if (source === 316) {
-        boundaries.push(new Boundary(j * 24, i * 24));
+        boundaries.push(new Boundary(j * 24, i * 24 + 1));
       }
     });
   });
@@ -63,14 +75,19 @@ function draw() {
   background(15);
   image(groundImage, 0, 0);
   for (let boundary of boundaries) {
-    if (rectangularCollision(player, boundary)) {
+    if (detectCollisions(player, boundary)) {
       player.stop();
-    }
+    } 
+    console.log(player.gravity)
+    // if(notCollided(player, boundary)) {
+    //   player.applyForce()
+    // }
     boundary.draw();
-  }  
+  }
   player.movement(pressedKey);
+
   player.getState();
   player.applyForce();
-  player.checkJump(boundaries[1]);
+  // player.checkJump(boundaries[1]);
   player.draw(charImage);
 }
